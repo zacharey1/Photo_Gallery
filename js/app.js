1a326a99d76e8552f7ad73703720943d5526fdef
zacharey1 <konohaha77@gmail.com>
2022-09-06 16:40:10 +0900

    js update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function search_photo() {
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase();
     let x = element.getAttribute('data-caption');
-    
+
     for (i = 0; i < x.length; i++) {
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
@@ -11,11 +11,3 @@ function search_photo() {
         }
     }
 }
-
-// for (i = 0; i < x.length; i++) {
-//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//         x[i].style.display="none";
-//     } else {
-//         x[i].style.display="list-item";
-//     }
-// }
