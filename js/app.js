25c0b918ce4d79513ec9135b26f5602a485cd272
zacharey1 <konohaha77@gmail.com>
2022-09-06 14:30:59 +0900

    css and js updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-baguetteBox.run('.gallery', {
-    // Custom options
-    captions,
-    buttons,
-    fullScreen,
-});
+function search_photo() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = element.getAttribute('data-caption');
+}
+
+for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display="none";
+    } else {
+        x[i].style.display="list-item";
+    }
+}
