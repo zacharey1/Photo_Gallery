8a7aab73cc0a601d1f207c3f5d7865f74ec74151
zacharey1 <konohaha77@gmail.com>
2022-09-06 16:38:00 +0900

    js update

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@ function search_photo() {
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase();
     let x = element.getAttribute('data-caption');
-}
-
-for (i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].style.display="none";
-    } else {
-        x[i].style.display="list-item";
+    
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        } else {
+            x[i].style.display="list-item";
+        }
     }
 }
+
+// for (i = 0; i < x.length; i++) {
+//     if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//         x[i].style.display="none";
+//     } else {
+//         x[i].style.display="list-item";
+//     }
+// }
