4b7f55f6eb9a809bf85f0e5bf5e9d8670e9828c9
zacharey1 <konohaha77@gmail.com>
2022-09-07 16:34:47 +0900

    js and html updates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,1 @@
-function search_photo() {
-    let input = document.getElementById('searchbar').value
-    input = input.toLowerCase();
-    let x = picture.getAttribute('data-caption');
-
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].toLowerCase().includes(input)) {
-            x[i].display="none";
-        } else {
-            x[i].display="list-item";
-        }
-    }
-}
+baguetteBox.run('.interactivegallery');
