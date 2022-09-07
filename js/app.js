29a8da3a751c7464f0dab8d5211557c58b55fe4e
zacharey1 <konohaha77@gmail.com>
2022-09-07 16:37:42 +0900

    js update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function() {
     baguetteBox.run('.gallery');
   });
+
+  document.getElementById("searchbar").addEventListener("keyup", getInput);
+            
+            function getInput() {
+              var x = document.getElementById("searchbar");
+              x.value = x.value.toLowerCase();
+              return x;
+            }
